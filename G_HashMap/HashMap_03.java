e05b2df02ee6015b4d088e742c9b4a2cb4af631e
Deepanshu Lanania <deepanshulavania69@gmail.com>
2024-08-28 13:27:35 +0530

    Check strings are isomorphic

--- G_HashMap/HashMap_03.java
+++ G_HashMap/HashMap_03.java
@@ -10,22 +10,20 @@ public class HashMap_03 {
         if (mp1.size() != mp2.size()) {
             return false;
         }
-
-        // Convert the keySet to a list
+        //? Convert the keySet to a list
         List<Character> keys = new ArrayList<>(mp1.keySet());
         System.out.println(keys);
         // Step 2: Use a normal for loop with an index to iterate over the keys
-        for (int i = 0; i < keys.size(); i++) {
+        for (int i = 0; i < keys.size(); i++) {//? forloop apply in array list, not in HashMap
             Character key = keys.get(i);
-            System.out.println("key is "+key);
-
+         
             if (!mp2.containsKey(key)) {
                 return false;  // Key not present in both maps
             }
             if (!mp1.get(key).equals(mp2.get(key))) {
                 return false;  // Values do not match
             }
-        }
+        }//? O(n)
 
         return true; // All keys and values match
     }
@@ -53,6 +51,66 @@ public class HashMap_03 {
         return areMapsEqual(mp1, mp2); 
     
     }
+
+    public static boolean isAnagramOneHashMap(String s , String t){
+        if (s.length()!=t.length()) {
+            return false;
+        }
+        HashMap<Character , Integer> mp1= freqString(s);  
+        for (int i = 0; i < t.length(); i++) {
+            Character ch= t.charAt(i);
+            if (!mp1.containsKey(ch)) {
+                return false;
+            }
+            int currFreq = mp1.get(ch);
+            mp1.put(ch, currFreq-1);
+            if (mp1.get(ch)==0) {
+                mp1.remove(ch);
+            }
+        }
+        //?This is to check value in hashmap :use extra space
+        // List<Character> keys = new ArrayList<>(mp1.keySet());
+        // System.out.println(keys);
+        // for (int i = 0; i < keys.size(); i++) {
+        //     Character key  = keys.get(i);
+        //     if (mp1.get(key)!=0) {
+        //         return false;
+        //     }
+        // }
+
+        //? this is to check value in map : Without using extra space
+        // for (var e : mp1.values()) {
+        //     if (e!=0) {
+        //         return false;
+        //     }
+        // }
+        if (mp1.size()!=0) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static boolean isIsomorphic(String s, String t){
+        if (s.length()!=t.length()) {
+            return false;
+        }
+        HashMap<Character, Character> mp=new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character sch =s.charAt(i);
+            Character tch =t.charAt(i);
+            if (mp.containsKey(sch)) {
+                if (mp.get(sch)!=tch) {
+                    return false;
+                }
+            }else if (mp.containsValue(tch)) {
+                return false;
+            }else{
+                mp.put(sch, tch);
+            }
+        }
+        
+        return true;
+    }
     public static void main(String args[]){
         //! Quse :(Valid Anagram) Check whether two Strings are anagram of each other. Return true if they are else false. An anagram  of a string is another string that contains the same characters, only the order of characters can be different . For ex: "abcd" and "dabc" are an anagram of each other.
         //? For ex: listen and silent (anagram) & anagram and grams (not anagram)
@@ -60,8 +118,18 @@ public class HashMap_03 {
 
         //? Condition : i> length of both (equal) ii> freq of each char (same) iii> No char extra from other
 
+        //?Approch 1:
         String s1= "abcd";
         String s2 = "dabc";
         System.out.println(isAnagram(s1, s2));
+        //We use Two hash map here due to which space are accumulate so much 
+
+        //? Approch2 : using One Hahs Map 
+        System.out.println("<========Using One HashMap========>");
+        System.out.println(isAnagramOneHashMap("keen", "knese"));//O(n)
+
+        //! Ques:Are two Strings isomorphic 
+        System.out.println("<======Check Strings are isomorphic =======>");
+        System.out.println(        isIsomorphic("abacd","xyxzx")        );
     }
 }
