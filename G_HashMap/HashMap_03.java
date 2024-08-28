2908377b3433913688e39f13613b7ff201a8d705
Deepanshu Lanania <deepanshulavania69@gmail.com>
2024-08-28 11:51:51 +0530

    Build  equals method for two hashMap

--- G_HashMap/HashMap_03.java
+++ G_HashMap/HashMap_03.java
@@ -1,8 +1,34 @@
 package G_HashMap;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class HashMap_03 {
+    public static boolean areMapsEqual(HashMap<Character, Integer> mp1, HashMap<Character, Integer> mp2) {
+        // Step 1: Check if sizes are equal
+        if (mp1.size() != mp2.size()) {
+            return false;
+        }
+
+        // Convert the keySet to a list
+        List<Character> keys = new ArrayList<>(mp1.keySet());
+        System.out.println(keys);
+        // Step 2: Use a normal for loop with an index to iterate over the keys
+        for (int i = 0; i < keys.size(); i++) {
+            Character key = keys.get(i);
+            System.out.println("key is "+key);
+
+            if (!mp2.containsKey(key)) {
+                return false;  // Key not present in both maps
+            }
+            if (!mp1.get(key).equals(mp2.get(key))) {
+                return false;  // Values do not match
+            }
+        }
+
+        return true; // All keys and values match
+    }
     public static HashMap<Character,Integer> freqString(String s){
         HashMap<Character , Integer> mp= new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
@@ -23,7 +49,8 @@ public class HashMap_03 {
         HashMap<Character , Integer> mp2= freqString(t);
         System.out.println(mp1.entrySet());
         System.out.println(mp2.entrySet());
-        return mp1.equals(mp2);  
+        // return mp1.equals(mp2); 
+        return areMapsEqual(mp1, mp2); 
     
     }
     public static void main(String args[]){
@@ -36,8 +63,5 @@ public class HashMap_03 {
         String s1= "abcd";
         String s2 = "dabc";
         System.out.println(isAnagram(s1, s2));
-
-         
-
     }
 }
