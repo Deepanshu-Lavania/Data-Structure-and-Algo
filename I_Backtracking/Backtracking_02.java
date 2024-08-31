fc05c123302bb1f43daad82d59b202a0aa8ebdca
Deepanshu Lanania <deepanshulavania69@gmail.com>
2024-08-31 16:00:25 +0530

    permutaion of array Using swapping (permutation method)

--- I_Backtracking/Backtracking_02.java
+++ I_Backtracking/Backtracking_02.java
@@ -45,6 +45,32 @@ public class Backtracking_02 {
         helper(nums,ds,isValid,ans);
         return ans;
     }
+    public static void swap(int i, int j, int nums[]){
+        int temp =nums[i];
+        nums[i] = nums[j];
+        nums[j] =temp;
+    }
+    public static  void help(int nums[],int idx, List<List<Integer>> ans){
+        int n=nums.length;
+        if (idx==n-1) {
+            List<Integer> l =new ArrayList<>();
+            for (int i = 0; i <n; i++) {
+                l.add(nums[i]);
+            }
+            ans.add(l);
+            return;
+        }
+        for (int i = idx; i < nums.length; i++) {
+            swap(i,idx,nums);
+            help(nums, idx+1, ans);//? Recursion
+            swap(i,idx,nums);
+        }
+    }
+    public static List<List<Integer>> permutation(int nums[]){
+        List<List<Integer>> ans = new ArrayList<>();
+        help(nums,0,ans);
+        return ans;
+    }
     public static void main(String[] args) {
         //! permutation of String 
         System.out.println("<====return all the possible permutations of String====>");
@@ -59,7 +85,12 @@ public class Backtracking_02 {
         System.out.println();
         System.out.println("<====return all the possible permutations of array====>");
         int arr[]={1,2,3};
+        //? Approch 1:
         List<List<Integer>> ans = permute(arr);
         System.out.println(ans);
+
+        //? Approch 2:
+        List<List<Integer>> arrList=permutation(arr);
+        System.out.println(arrList);
     }
 }
