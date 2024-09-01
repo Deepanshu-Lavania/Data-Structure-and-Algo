f41b2909399774f53fb0327303b3cd6310e47230
Deepanshu Lanania <deepanshulavania69@gmail.com>
2024-09-01 12:05:06 +0530

    maximum no. of knights that can be placed in a given nXn

--- I_Backtracking/Backtracking_03.java
+++ I_Backtracking/Backtracking_03.java
@@ -55,7 +55,7 @@ public class Backtracking_03 {
                 System.out.println();
             }
             System.out.println();
-            return ;
+            return;
         }
         // work
         for (int j = 0; j < n; j++) {
@@ -66,15 +66,16 @@ public class Backtracking_03 {
             }
         }
     }
-    public static void nqueenList(char board[][], int row,List<List<String>> ans) {
+
+    public static void nqueenList(char board[][], int row, List<List<String>> ans) {
         int n = board.length;
         // base condition
         if (row == n) {
-            List<String> l=new ArrayList<>();
+            List<String> l = new ArrayList<>();
             for (int i = 0; i < board.length; i++) {
-                String str="";
+                String str = "";
                 for (int j = 0; j < board.length; j++) {
-                    str=str+board[i][j];
+                    str = str + board[i][j];
                 }
                 l.add(str);
             }
@@ -85,9 +86,154 @@ public class Backtracking_03 {
         for (int j = 0; j < n; j++) {
             if (isSafe(board, row, j)) {
                 board[row][j] = 'Q';// visited
-                nqueenList(board, row + 1,ans);
+                nqueenList(board, row + 1, ans);
                 board[row][j] = '.';// Backtracking
             }
+        }
+    }
+
+    public static boolean knightHelper(int grid[][], int row, int col, int num) {
+        int n = grid.length;
+        // base condition
+        if (grid[row][col] == n * n - 1)
+            return true;
+        // 2 Up 1 right
+        int i, j;
+        i = row - 2;
+        j = col + 1;
+        if (i >= 0 && j < n) {
+            if (grid[i][j] == num + 1)
+                return knightHelper(grid, i, j, num + 1);
+        }
+        // 2 Up 1 left
+        i = row - 2;
+        j = col - 1;
+        if (i >= 0 && j >= 0 && grid[i][j] == num + 1)
+            return knightHelper(grid, i, j, num + 1);
+        // 2 Down 1 right
+        i = row + 2;
+        j = col + 1;
+        if (i < n && j < n && grid[i][j] == num + 1)
+            return knightHelper(grid, i, j, num + 1);
+        // 2 Down 1 left
+        i = row + 2;
+        j = col - 1;
+        if (i < n && j >= 0 && grid[i][j] == num + 1)
+            return knightHelper(grid, i, j, num + 1);
+        // 2 Right 1 up
+        i = row - 1;
+        j = col + 2;
+        if (i >= 0 && j < n && grid[i][j] == num + 1)
+            return knightHelper(grid, i, j, num + 1);
+        // 2 Right 1 down
+        i = row + 1;
+        j = col + 2;
+        if (i < n && j < n && grid[i][j] == num + 1)
+            return knightHelper(grid, i, j, num + 1);
+        // 2 Left 1 up
+        i = row - 1;
+        j = col - 2;
+        if (i >= 0 && j >= 0 && grid[i][j] == num + 1)
+            return knightHelper(grid, i, j, num + 1);
+        // 2 Left 1 down
+        i = row + 1;
+        j = col - 2;
+        if (i < n && j >= 0 && grid[i][j] == num + 1)
+            return knightHelper(grid, i, j, num + 1);
+
+        return false;
+    }
+
+    public static boolean checkValidKnight(int grid[][]) {
+        if (grid[0][0] != 0)
+            return false;
+        return knightHelper(grid, 0, 0, 0);
+    }
+
+    public static boolean isknightSafe(char grid[][], int row, int col) {
+        int n = grid.length;
+        // 2 Up 1 right
+        int i, j;
+        i = row - 2;
+        j = col + 1;
+        if (i >= 0 && j < n) {
+            if (grid[i][j] == 'k')
+                return false;
+        }
+        // 2 Up 1 left
+        i = row - 2;
+        j = col - 1;
+        if (i >= 0 && j >= 0 && grid[i][j] == 'k')
+            return false;
+        // 2 Down 1 right
+        i = row + 2;
+        j = col + 1;
+        if (i < n && j < n && grid[i][j] == 'k')
+            return false;
+        // 2 Down 1 left
+        i = row + 2;
+        j = col - 1;
+        if (i < n && j >= 0 && grid[i][j] == 'k')
+            return false;
+        // 2 Right 1 up
+        i = row - 1;
+        j = col + 2;
+        if (i >= 0 && j < n && grid[i][j] == 'k')
+            return false;
+        // 2 Right 1 down
+        i = row + 1;
+        j = col + 2;
+        if (i < n && j < n && grid[i][j] == 'k')
+            return false;
+        // 2 Left 1 up
+        i = row - 1;
+        j = col - 2;
+        if (i >= 0 && j >= 0 && grid[i][j] == 'k')
+            return false;
+        // 2 Left 1 down
+        i = row + 1;
+        j = col - 2;
+        if (i < n && j >= 0 && grid[i][j] == 'k')
+            return false;
+        return true;
+    }
+
+    static int maxKnights = 0;
+
+    public static void placedKnight(char arr[][], int row, int col, int number) {
+        int n = arr.length;
+        // Base condition: End of rows
+        if (row == n) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(arr[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+            if (number > maxKnights) {
+                maxKnights = number;
+            }
+
+            return;
+        }
+
+        // Check if the knight can be placed at (row, col)
+        if (isknightSafe(arr, row, col)) {
+            arr[row][col] = 'k'; // Place the knight
+            if (col != n - 1) {
+                placedKnight(arr, row, col + 1, number + 1);
+            } else {
+                placedKnight(arr, row + 1, 0, number + 1);
+            }
+            arr[row][col] = 'x'; // Backtrack
+        }
+
+        // Move to the next cell without placing a knight
+        if (col != n - 1) {
+            placedKnight(arr, row, col + 1, number);
+        } else {
+            placedKnight(arr, row + 1, 0, number);
         }
     }
 
@@ -111,7 +257,40 @@ public class Backtracking_03 {
                 chessBoard[i][j] = '.';
             }
         }
-        nqueenList(chessBoard, 0,ans);
+        nqueenList(chessBoard, 0, ans);
         System.out.println(ans);
+
+        // ! Ques : Check Knight Tour Configuration [2596]
+        /*
+         * There is a knight on an n x n chessboard. In a valid configuration,
+         * the knight starts at the top-left cell of the board and visits every cell on
+         * the board exactly once.
+         * You are given an n x n integer matrix grid consisting of distinct integers
+         * from the range [0, n * n - 1] where grid[row][col] indicates that the cell
+         * (row, col) is the grid[row][col]th cell that the knight visited. The moves
+         * are 0-indexed.
+         * Return true if grid represents a valid configuration of the knight's
+         * movements or false otherwise.
+         */
+        System.out.println("<======Check Knight Tour Configuration=====>");
+        int grid[][] = { { 0, 11, 16, 5, 20 }, { 17, 4, 19, 10, 15 }, { 12, 1, 8, 21, 6 }, { 3, 18, 23, 14, 9 },
+                { 24, 13, 2, 7, 22 } };
+        System.out.println(checkValidKnight(grid));
+
+        System.out.println("<=====find maximum no. of knights that can be placed=====>");
+        // ! Ques : Tell the maximum no. of knights that can be placed in a given nXn
+        // chessboard
+        int d =3;
+        char[][] arr = new char[d][d];
+        int number = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = 'x';
+            }
+        }
+
+        placedKnight(arr, 0, 0, number);
+        System.out.println("Maximum number of knights that can be placed: " + maxKnights);
     }
 }
