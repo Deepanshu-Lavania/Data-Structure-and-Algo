f6706a7bad258964a08d63c0866172c16f2427c4
Deepanshu Lanania <deepanshulavania69@gmail.com>
2024-09-01 09:54:35 +0530

    n-queen Problem using List of ArrayList

--- I_Backtracking/Backtracking_03.java
+++ I_Backtracking/Backtracking_03.java
@@ -1,5 +1,7 @@
 package I_Backtracking;
 
+import java.util.*;
+
 public class Backtracking_03 {
     public static boolean isSafe(char board[][], int row, int col) {
         int n = board.length;
@@ -53,7 +55,7 @@ public class Backtracking_03 {
                 System.out.println();
             }
             System.out.println();
-            return;
+            return ;
         }
         // work
         for (int j = 0; j < n; j++) {
@@ -63,12 +65,35 @@ public class Backtracking_03 {
                 board[row][j] = 'X';// Backtracking
             }
         }
-
+    }
+    public static void nqueenList(char board[][], int row,List<List<String>> ans) {
+        int n = board.length;
+        // base condition
+        if (row == n) {
+            List<String> l=new ArrayList<>();
+            for (int i = 0; i < board.length; i++) {
+                String str="";
+                for (int j = 0; j < board.length; j++) {
+                    str=str+board[i][j];
+                }
+                l.add(str);
+            }
+            ans.add(l);
+            return;
+        }
+        // work
+        for (int j = 0; j < n; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';// visited
+                nqueenList(board, row + 1,ans);
+                board[row][j] = '.';// Backtracking
+            }
+        }
     }
 
     public static void main(String[] args) {
         // ! Quse: n-queen Problem
-        System.out.println("<======n-queen Problem========>");
+        System.out.println("<======n-queen Problem using Array========>");
         int n = 4;
         char board[][] = new char[n][n];
         for (int i = 0; i < n; i++) {
@@ -77,5 +102,16 @@ public class Backtracking_03 {
             }
         }
         nqueen(board, 0);
+        System.out.println("<======n-queen Problem using List of ArrayList========>");
+        int m = 4;
+        List<List<String>> ans = new ArrayList<>();
+        char chessBoard[][] = new char[m][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                chessBoard[i][j] = '.';
+            }
+        }
+        nqueenList(chessBoard, 0,ans);
+        System.out.println(ans);
     }
 }
