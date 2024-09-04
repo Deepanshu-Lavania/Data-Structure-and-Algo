2d8c33680a0f00dab6144659f688231c4878462b
Deepanshu Lanania <deepanshulavania69@gmail.com>
2024-09-04 16:36:52 +0530

    The sum of nodes at any level in tree

--- J_Tree/BinaryTree_02.java
+++ J_Tree/BinaryTree_02.java
@@ -1,5 +1,7 @@
 package J_Tree;
 
+import java.util.*;
+
 public class BinaryTree_02 {
     public static class Node {
         Node leftNode;
@@ -27,6 +29,7 @@ public class BinaryTree_02 {
             return newNode;
         }
     }
+
     public static class BinarySubTrees {
         private int idx = -1; // Make idx an instance variable
 
@@ -41,7 +44,7 @@ public class BinaryTree_02 {
             return newNode;
         }
     }
-    
+
     public static int maxHeight(Node root) {
         if (root == null) {
             return 0;
@@ -56,67 +59,68 @@ public class BinaryTree_02 {
             return rightheight + 1;
         }
     }
-    public static int maxDiam(Node root){
-        if (root==null) {
+
+    public static int maxDiam(Node root) {
+        if (root == null) {
             return 0;
         }
         int Diam1 = maxDiam(root.leftNode);
-        int Diam2 =maxDiam(root.rightNode);
-        int Diam3 = maxHeight(root.leftNode)+maxHeight(root.rightNode)+1;
+        int Diam2 = maxDiam(root.rightNode);
+        int Diam3 = maxHeight(root.leftNode) + maxHeight(root.rightNode) + 1;
 
         return Math.max(Diam3, Math.max(Diam1, Diam2));
     }
 
-
-    public static class TreeInfo{//this class is used so that we can return two data 
+    public static class TreeInfo {// this class is used so that we can return two data
         int ht;
         int diam;
-        TreeInfo(int ht, int diam){//constructor
-            this.ht=ht;
-            this.diam=diam;
+
+        TreeInfo(int ht, int diam) {// constructor
+            this.ht = ht;
+            this.diam = diam;
         }
     }
 
-    public static TreeInfo  diameter2(Node root){
-        if (root==null) {
-            return new TreeInfo(0,0);
+    public static TreeInfo diameter2(Node root) {
+        if (root == null) {
+            return new TreeInfo(0, 0);
         }
-        
+
         TreeInfo leftSubtree = diameter2(root.leftNode);
         TreeInfo rightSubtree = diameter2(root.rightNode);
 
-        //height
-        int myHeight = Math.max(leftSubtree.ht, rightSubtree.ht)+1;
+        // height
+        int myHeight = Math.max(leftSubtree.ht, rightSubtree.ht) + 1;
 
-        //diameter for all cases : leftSubtree , rightSubtree , wholeTree
+        // diameter for all cases : leftSubtree , rightSubtree , wholeTree
         int diam1 = leftSubtree.diam;
-        int diam2= rightSubtree.diam;
-        int diam3= leftSubtree.ht+rightSubtree.ht+1;
+        int diam2 = rightSubtree.diam;
+        int diam3 = leftSubtree.ht + rightSubtree.ht + 1;
 
         int mydiam = Math.max(Math.max(diam1, diam2), diam3);
-        
-        TreeInfo myInfo= new TreeInfo(myHeight, mydiam);
+
+        TreeInfo myInfo = new TreeInfo(myHeight, mydiam);
         return myInfo;
     }
 
-
-    public static boolean isIdentical(Node root , Node subroot){
-        if (root==null && subroot==null) {
+    public static boolean isIdentical(Node root, Node subroot) {
+        if (root == null && subroot == null) {
             return true;
         }
-        if (root ==null || subroot==null) {
+        if (root == null || subroot == null) {
             return false;
         }
-        if (root.val==subroot.val) {
-            return isIdentical(root.leftNode, subroot.leftNode)&& isIdentical(root.rightNode, subroot.rightNode);    
+        if (root.val == subroot.val) {
+            return isIdentical(root.leftNode, subroot.leftNode) && isIdentical(root.rightNode, subroot.rightNode);
         }
         return false;
     }
-    public static boolean checkSubtree(Node root,Node subroot){
-        if (subroot==null) {
+
+    public static boolean checkSubtree(Node root, Node subroot) {
+        if (subroot == null) {
             return true;
         }
-        if (root==null) {
+        if (root == null) {
             return false;
         }
         if (isIdentical(root, subroot)) {
@@ -125,6 +129,42 @@ public class BinaryTree_02 {
             }
         }
         return checkSubtree(root.leftNode, subroot) || checkSubtree(root.rightNode, subroot);
+    }
+
+    public static int levelSum(Node root,int k) {
+        if (root == null)
+            return 0;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        int sum=0;
+        int count=1;
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (count==k && currNode==null) {
+                return sum;
+            }
+            if (currNode == null) {
+                sum=0;
+                count++;
+                if (!q.isEmpty()) {
+                    q.add(null);
+                }else{
+                    break;
+                }
+            }else{
+                if (k==count) {
+                    sum+=currNode.val;
+                }
+                if (currNode.leftNode!=null) {
+                    q.add(currNode.leftNode);
+                }
+                if (currNode.rightNode!=null) {
+                    q.add(currNode.rightNode);
+                }
+            }
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
@@ -139,21 +179,28 @@ public class BinaryTree_02 {
             System.out.println("The tree is empty.");
         }
 
-        //! Ques : Find largest diameter of a tree
+        // ! Ques : Find largest diameter of a tree
         System.out.println("<=======Find largest diameter of a tree in Quadratic time ======>");
         System.out.println(maxDiam(root));
-        //? Time complexity is O(n^2) because we traverse for diameter on each row then we also traverse again for height calculation.
+        // ? Time complexity is O(n^2) because we traverse for diameter on each row then
+        // we also traverse again for height calculation.
 
         // diameter2(root);
         System.out.println("Find largest diameter of a tree in linear time");
         System.out.println(diameter2(root).diam);
 
-        //! Ques : Check subtree is present in the Tree 
-        int subnode[]={7, 8, -1, 9, 10, -1, -1};
-        Node subroot=subtree.buildSubTrees(subnode);
+        // ! Ques : Check subtree is present in the Tree
+        int subnode[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Node subroot = subtree.buildSubTrees(subnode);
         System.out.println("<======Is Subtree present in the Tree =====>");
-        checkSubtree(root,subroot);
+        checkSubtree(root, subroot);
         System.out.println(checkSubtree(root, subroot));
+
+        System.out.println();
+        System.out.println("<=====Levelorder Sum=====>");
+        int k=3;
+        int sum =levelSum(subroot,k);
+        System.out.println("The sum of tree at level "+k +" is "+sum);
     }
 
 }
