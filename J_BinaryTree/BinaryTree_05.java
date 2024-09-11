b9d380d3cb466f71b1112379cadfcec3650221e4
Deepanshu Lanania <deepanshulavania69@gmail.com>
2024-09-11 14:35:41 +0530

    Zig-Zag printing using nth level

--- J_BinaryTree/BinaryTree_05.java
+++ J_BinaryTree/BinaryTree_05.java
@@ -1,5 +1,8 @@
 package J_BinaryTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BinaryTree_05 {
     public static class Node{
@@ -49,11 +52,42 @@ public class BinaryTree_05 {
         if(root==null) return true;
         return isSameTree(root.left, invertTree(root.right));
     }
+
+   public static int height(Node root) {
+        if (root == null)
+            return 0;
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+
+    public static void nthLevel(Node root, int n, List<Integer> arr) {
+        if (root == null) {
+            return;
+        }
+        if (n == 1) {
+            arr.add(root.val);
+            return;
+        }
+        nthLevel(root.left, n - 1, arr);
+        nthLevel(root.right, n - 1, arr);
+    }
+
+    public static void nthLevel2(Node root, int n, List<Integer> arr) {
+        if (root == null) {
+            return;
+        }
+        if (n == 1) {
+            arr.add(root.val);
+            return;
+        }
+        nthLevel2(root.right, n - 1, arr);
+        nthLevel2(root.left, n - 1, arr);
+    }
     public static void main(String[] args) {
         BinaryTrees tree = new BinaryTrees();
         // int node[] = { 5, 2, 3, -1, -1, 4, -1, -1, 1, 6, -1, -1, 7, -1, -1 };
             // int node[]={1,2,-1,-1,3,-1,-1};
-        int node[] = {1,2,3,-1,-1,4,-1,-1,2,4,-1,-1,3,-1,-1};
+        // int node[] = {1,2,3,-1,-1,4,-1,-1,2,4,-1,-1,3,-1,-1};
+        int node[] = {1,3,6,-1,-1,7,-1,-1,2,5,-1,-1,4,-1,-1};
         Node root = tree.buildTrees(node);
         if (root != null) {
             System.out.println(root.val +" root.left "+root.left.val +" && root.right "+root.right.val); // Prints the value of the root node
@@ -73,5 +107,20 @@ public class BinaryTree_05 {
             System.out.println("Tree is not Symmetric");
         }
 
+        //! Ques : Zig-Zag printing
+
+        System.out.println("<====Zig-Zag printing using nth level====>");
+        int level = height(root);
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 1; i <= level; i++) {
+            List<Integer> arr = new ArrayList<>();
+            if (i % 2 == 0) {
+                nthLevel2(root, i, arr);
+            } else {
+                nthLevel(root, i, arr);
+            }
+            list.add(arr);
+        }
+        System.out.println(list);
     }
 }
