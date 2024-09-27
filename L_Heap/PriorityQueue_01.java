1b0694128b7b7f0a97dd24a9f914939bb2618b57
Deepanshu Lanania <deepanshulavania69@gmail.com>
2024-09-27 11:32:08 +0530

    Find closest distance from origin for kth number

---
+++ L_Heap/PriorityQueue_01.java
@@ -0,0 +1,70 @@
+package L_Heap;
+import java.util.*;
+
+public class PriorityQueue_01 {
+    public static class Student {
+        String name;
+        String dob;
+        int roolno;
+        int marks;
+    }
+    //comparator<Type> : inbuilt class
+    public static class StudentComparator implements Comparator<Student>{
+        //overriding
+        public int compare(Student s1, Student s2){
+            if (s1.marks>s2.marks) {
+                return 1;
+            }else if (s1.marks<s2.marks) {
+                return -1;
+            }else{
+                if (s1.roolno>s2.roolno) {
+                    return 1;
+                }else if (s1.roolno<s2.roolno) {
+                    return -1;
+                }else{
+                    return 0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());//? by default in ascending order
+
+        /* PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(10);
+        pq.add(20);
+        pq.add(11);
+        pq.add(15);
+        pq.add(2);
+        pq.add(50);
+        System.out.println("Before Remove ====> ");
+        System.out.println("Peek Elem : "+pq.peek());
+        System.out.println(pq);
+        System.out.println("After Remove ====> ");
+        pq.remove();
+        System.out.println("Peek Elem : "+pq.peek());
+        System.out.println(pq); */
+
+        PriorityQueue<Student> studentpq = new PriorityQueue<>(new StudentComparator());//when we have sns string data to compare then we have to use comparator so that we compare the value of String 
+        //it is compare throgh our own make logic
+
+        Student harsh = new Student();
+        harsh.name="harsh";
+        harsh.dob="12-01-2004";
+        harsh.marks=99;
+        harsh.roolno=123;
+
+        Student raghav = new Student();
+        raghav.name="raghav";
+        raghav.dob="22-01-2006";
+        raghav.marks=89;
+        raghav.roolno=223;
+
+        studentpq.add(harsh);
+        studentpq.add(raghav);
+
+        System.out.println(studentpq.peek().name);
+        System.out.println(studentpq.peek().roolno);
+        System.out.println(studentpq.peek().dob);
+    }
+}
---
+++ L_Heap/PriorityQueue_02.java
@@ -0,0 +1,97 @@
+package L_Heap;
+
+import java.util.*;
+
+public class PriorityQueue_02 {
+    public static class Point {
+        int x;
+        int y;
+        int distance;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.distance = x * x + y * y;// calculate sqared distance from the origin
+        }
+    }
+
+    public static class PointComparator implements Comparator<Point> {
+        public int compare(Point p1, Point p2) {//? compare of distace
+            if (p1.distance < p2.distance)// Max-Heap: Larger distances should come first
+                return 1;
+            else if (p1.distance > p2.distance)
+                return -1;
+            else
+                return 0;
+        }
+    }
+    
+    public static void kthclosestPoints(int x1[],int y1[]){
+        PriorityQueue<Point> cordi = new PriorityQueue<>(new PointComparator());
+
+        int kitem = 3; // Let's say we want the 3 closest points
+
+        // Insert the first set of points into the heap
+        for (int i = 0; i < x1.length; i++) {
+            Point p = new Point(x1[i], y1[i]);
+            cordi.offer(p);//add element into priorityQueue
+            if (cordi.size() > kitem) {
+                cordi.poll(); // Remove the point with the largest distance
+            }
+        }
+
+        // Output the kitem closest points
+        System.out.println("The " + kitem + " closest points to the origin are:");
+        while (!cordi.isEmpty()) {
+            Point p = cordi.poll();
+            System.out.println("(" + p.x + ", " + p.y + ") with distance: " + p.distance);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>();// don't change original array
+        int k = 3;
+        arr.add(2);
+        arr.add(7);
+        arr.add(8);
+        arr.add(9);
+        arr.add(5);
+        arr.add(1);
+
+        // ! Ques : Given an array integer nums and an integer k, return the kth largest
+        // element in the array. Note that it is the kth largest element in the largest
+        // element in the sorted order, not the kth distinct element.
+        System.out.println("<====Find kth largest element==========>");
+        // ? Brut Force method(Approch1)
+        List<Integer> sortedArr = new ArrayList<>(arr);// SC : O(n)
+        sortedArr.sort(Comparator.reverseOrder());// T.C : O(logn)
+        System.out.println(sortedArr.get(2));// 0th based indexing and T.C:O(1)
+
+        // ? Better/Optimized (Approch2)
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < arr.size(); i++) {
+            // if top smallest, remove top and put largest
+            if (pq.size() == k) {
+                if (pq.peek() < arr.get(i)) {
+                    pq.remove();
+                    pq.add(arr.get(i));
+                }
+                continue;
+            }
+            // add ele
+            pq.add(arr.get(i));
+        }
+        System.out.println(pq.peek());
+        // ?Time complexity : O(nlogk), where n is the size of the array arr and k is
+        // the size of the PriorityQueue and Space Complexity : O(k)
+
+        // ! Ques : given a set of points in a XY plane . you need to find the kth closest points to the origin in which no 2 point distace is same from the origin
+        // Points: (5,3), (1,2), (3,4), (5,6), (7,8), (9,10)
+        System.out.println("<====== Find the kth closest points to the origin =====>");
+        int x1[] = { 5, 1, 3, 5, 7, 9 };
+        int y1[] = { 3, 2, 4, 6, 8, 10 };
+        kthclosestPoints(x1,y1);
+
+    }
+}
